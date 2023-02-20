6a9a1e7f9c3cf6178dbf4e88101983eee6a7d91e
Rutujadd27 <rutujadd27@gmail.com>
2023-02-20 17:54:41 +0530

    done

---
+++ Backend/model/user.model.js
@@ -0,0 +1,17 @@
+const mongoose=require("mongoose")
+
+const userSchema=mongoose.Schema({
+    name:String,
+    pass:String,
+    email:String,
+    
+})
+
+const UserModel=mongoose.model("user",userSchema)
+
+module.exports={
+    UserModel
+}
+
+
+
